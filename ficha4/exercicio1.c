563d0bcc069d0213377e6c14491c05c6c9262eb7
João Pedro Oliveira <1212236@isep.ipp.pt>
2022-11-16 11:53:22 +0000

    initial commit

---
+++ ficha4/exercicio1.c
@@ -0,0 +1,59 @@
+/* 
+Ficha #4 - Exercicio #1
+
+a) Este programa tem como objetivo a leitura de três valores inteiros (horas, minutos e segundos) e a apresentação do resultado da soma desses valores em segundos. Para tal, o programa deve ler os valores e calcular a soma dos segundos, apresentando o resultado no ecrã.
+
+*/
+
+
+#include <stdio.h>
+
+int conversao(int h, int m, int s)
+{
+    int seg;
+    seg = h*3600+m*60+s;
+    return seg;
+}
+
+/*
+b) Esta funcao recebe dois valores inteiros, um limite inferior e um limite superior, e solicita ao utilizador a introdução de um número inteiro entre esses limites sempre que o utilizador não o faça. A função deve devolver o número inteiro introduzido pelo utilizador.
+*/
+
+int leitura (int limiteInferior, int limiteSuperior) {
+    int resultado; // A variável resultado é declarada mas não necessita ser inicializada uma vez que o valor é atribuido no scanf.
+
+    // loop que é executado enquanto o valor introduzido pelo utilizador não estiver entre os limites.
+    do {
+        scanf("%i", &resultado); // A variável resultado é lida e atribuida ao valor introduzido pelo utilizador.
+
+        if (resultado < limiteInferior || resultado > limiteSuperior) {
+            // se o valor introduzido pelo utilizador não estiver entre os limites, é apresentada uma mensagem de erro a indicar entre quais limites o número introduzido se deve situar.
+            printf("Valor invalido, por favor insira um valor entre %i e %i: ", limiteInferior, limiteSuperior);
+        };
+    } while (resultado < limiteInferior || resultado > limiteSuperior);
+
+    return resultado; // A função devolve o valor introduzido pelo utilizador.
+}
+
+int main( void ) {
+    int hor, min, seg, segundos;
+
+    // as variáveis hor, min e seg são lidas e atribuidas aos valores introduzidos pelo utilizador. O recurso à função da stdlib 'scanf' foi substituida pela nossa função 'leitura' que verifica se o valor introduzido pelo utilizador está entre os limites.
+
+    printf("Digite as horas: ");
+    hor = leitura (0, 24);
+
+    printf("Digite os minutos: ");
+    min = leitura (0, 60);
+
+    printf("Digite os segundos: ");
+    seg = leitura (0, 60);
+
+    // no final é feita a conversão dos valores introduzidos pelo utilizador para segundos e é apresentado o resultado no ecrã.
+
+    segundos = conversao(hor,min,seg);
+    printf("Total em segundos: %i \n", segundos);
+
+    // por convenção o valor de retorno de uma função main é 0 quando esta termina sem erros.
+    return 0;
+}
---
+++ ficha4/exercicio2.c
@@ -0,0 +1,67 @@
+/*
+Ficha #4 - Exercicio #2
+
+Escreva um programa em linguagem C que calcule e mostre o N-ésimo número natural cuja soma dos seus algarismos seja igual a X.
+O cálculo da soma dos algarismos de um número deverá ser efetuado numa função específica.
+O valor N e X devem ser pedidos ao utilizador.
+
+Penso que a pergunta é um pouco ambígua mas vou assumir que o N-ésimo número natural é um número inteiro cuja soma dos algarismos individuais é igual a X.
+
+Por exemplo: 22 é o 3º número natural cuja soma dos algarismos é igual a 4.
+
+*/
+
+#include <stdio.h> // a biblioteca stdio.h é necessária para a função printf e scanf.
+
+// a função somaDeDigitos recebe um número inteiro e devolve a soma dos seus algarismos.
+
+int somaDeDigitos(int numeroEscolhido) {
+    int soma = 0; // a variável soma é declarada e inicializada a 0.
+
+    // o loop é executado enquanto o número escolhido for maior que 0. Em cada iteração o número escolhido é dividido por 10 e o resto da divisão é adicionado à soma.
+    while (numeroEscolhido > 0) {
+        // a variável soma é incrementada com o resto da divisão do número escolhido por 10.
+        soma += numeroEscolhido % 10;
+        // o número escolhido é dividido por 10.
+        numeroEscolhido /= 10;
+    }
+
+    // a função devolve o valor da variável soma.
+    return soma;
+}
+
+int main () {
+
+    // as variáveis numeroDeIteracoes e numeroEscolhido são declaradas sem serem inicializadas uma vez que o seu valor é atribuido no scanf.
+    int numeroDeIteracoes; 
+    int numeroEscolhido;
+
+    // as variáveis combinacoesEncontradas e iteracao são declaradas e inicializadas a 0.
+    int combinacoesEncontradads = 0;
+    int iteracao = 0;
+
+    // é solicitado ao utilizador que introduza o número de iterações.
+    printf("Por favor introduza o numero de iteracoes desejadas: ");
+    scanf("%i", &numeroDeIteracoes);
+
+    // é solicitado ao utilizador que introduza o número desejado.
+    printf("Por favor introduza o numero a ser comparado: ");
+    scanf("%i", &numeroEscolhido);
+
+    // o loop é executado enquanto o número de iterações for maior que o número de combinações encontradas. Escolhi um loop while porque a execução do loop está dependente da realação do numero de iterações com o numero de combinações encontradas e não com o numero da iteração.
+
+    do {
+        int somaTemporaria = somaDeDigitos(iteracao); // a variável somaTemporaria é declarada e inicializada com o valor da função somaDeDigitos com o numero da iteração atual como parâmetro.
+
+        if (somaTemporaria == numeroEscolhido) {
+            // se a soma temporária for igual ao número escolhido, o número de combinações encontradas é incrementado em 1 e o utilizador é informado do número da combnação e do valor cuja soma dos algoritmos corresponde ao número escolhido pelo utilizador.
+            combinacoesEncontradads++;
+            printf("Combinacao valida #%i: %i\n", combinacoesEncontradads, iteracao);
+        }
+        // o número da iteração é incrementado em 1.
+        iteracao++;
+    } while (numeroDeIteracoes != combinacoesEncontradads);
+
+    // por convenção o valor de retorno de uma função main é 0 quando esta termina sem erros.
+    return 0;
+}
